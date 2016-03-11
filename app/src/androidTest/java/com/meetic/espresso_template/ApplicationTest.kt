18dd41700f3a5eb283eb463b68a26d44fd19b978
Geoffrey TAN <g.tan@meetic-corp.com>
2016-03-11 17:12:51 +0100

    Initial project setup

---
+++ app/src/androidTest/java/com/meetic/espresso_template/ApplicationTest.kt
@@ -0,0 +1,9 @@
+package com.meetic.espresso_template
+
+import android.app.Application
+import android.test.ApplicationTestCase
+
+/**
+ * [Testing Fundamentals](http://d.android.com/tools/testing/testing_android.html)
+ */
+class ApplicationTest : ApplicationTestCase<Application>(Application::class.java)
