aa9c22cb00f4f8dfea9da7eeae055b3cfaae512b
c.creusot <c.creusot@meetic-corp.com>
2016-04-26 11:39:30 +0200

    Complete the login.feature as steps definition.

--- app/src/androidTest/java/com/meetic/espresso_template/test/steps/StepsDefinitions.kt
+++ app/src/androidTest/java/com/meetic/espresso_template/test/steps/StepsDefinitions.kt
@@ -3,18 +3,26 @@ package com.meetic.espresso_template.test.steps
 import android.support.test.InstrumentationRegistry
 import android.support.test.espresso.Espresso
 import android.support.test.espresso.action.ViewActions
+import android.support.test.espresso.assertion.ViewAssertions
+import android.support.test.espresso.intent.Intents
+import android.support.test.espresso.intent.matcher.IntentMatchers
 import android.support.test.espresso.matcher.ViewMatchers
 import android.test.ActivityInstrumentationTestCase2
-
+import android.view.View
+import android.widget.EditText
+import com.meetic.espresso_template.ListActivity
 import com.meetic.espresso_template.LoginActivity
 import com.meetic.espresso_template.R
 import com.meetic.espresso_template.test.tools.ActivityKiller
-
 import cucumber.api.java.After
 import cucumber.api.java.Before
+import cucumber.api.java.en.And
 import cucumber.api.java.en.Given
+import cucumber.api.java.en.Then
 import cucumber.api.java.en.When
-import kotlinx.android.synthetic.main.login_activity.*
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.junit.internal.matchers.TypeSafeMatcher
 
 /**
  * *  Meetic
@@ -27,13 +35,16 @@ class StepsDefinitions : ActivityInstrumentationTestCase2<LoginActivity>(LoginAc
     public override fun setUp() {
         super.setUp()
 
+
         injectInstrumentation(InstrumentationRegistry.getInstrumentation())
         activity
+        Intents.init()
     }
 
     @After
     @Throws(Exception::class)
     public override fun tearDown() {
+        Intents.release()
         ActivityKiller.killOpenActivities()
         super.tearDown()
     }
@@ -47,5 +58,35 @@ class StepsDefinitions : ActivityInstrumentationTestCase2<LoginActivity>(LoginAc
     fun I_enter_my_credentials_login_and_password(login : String?, password : String?) {
         Espresso.onView(ViewMatchers.withId(R.id.emailEditTextView)).perform(ViewActions.typeText(login))
         Espresso.onView(ViewMatchers.withId(R.id.passwordEditTextView)).perform(ViewActions.typeText(password), ViewActions.closeSoftKeyboard())
+    }
+
+    @And("I press login button")
+    fun I_press_login_button() {
+        Espresso.onView(ViewMatchers.withId(R.id.loginButton)).perform(ViewActions.click())
+    }
+
+    @Then("I see (.*)$")
+    fun I_see(result : String) {
+        when (result) {
+            "errors" -> {
+                Espresso.onView(ViewMatchers.withId(R.id.emailEditTextView)).check(ViewAssertions.matches(error()))
+            }
+            "snackbar error" -> {
+                Espresso.onView(ViewMatchers.withText("Invalid user")).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            }
+            "Logged screen" -> {
+                Intents.intended(IntentMatchers.hasComponent(ListActivity::class.java.name))
+            }
+        }
+    }
+
+    fun error() : Matcher<View> = object : TypeSafeMatcher<View>() {
+        override fun describeTo(description: Description?) {
+            // do nothing
+        }
+
+        override fun matchesSafely(item: View?): Boolean {
+            return if (item is EditText) item.error != null else false
+        }
     }
 }
--- app/src/main/java/com/meetic/espresso_template/LoginActivity.kt
+++ app/src/main/java/com/meetic/espresso_template/LoginActivity.kt
@@ -1,8 +1,9 @@
 package com.meetic.espresso_template
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.login_activity.*
 
 class LoginActivity : AppCompatActivity() {
@@ -11,9 +12,12 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
 
-        loginButton.setOnClickListener{
+        loginButton.setOnClickListener {
             v ->
-            if (emailEditTextView.text.isNotEmpty() && passwordEditTextView.text.isNotEmpty()) {
+            if (emailEditTextView.text.contains("user2") && passwordEditTextView.text.contains("password2")) {
+                val snack = Snackbar.make(v, "Invalid user", 3000);
+                snack.show();
+            } else if (emailEditTextView.text.isNotEmpty() && passwordEditTextView.text.isNotEmpty()) {
                 startActivity(Intent(this, ListActivity::class.java));
             } else {
                 emailEditTextView.error = "You should fill the email";
